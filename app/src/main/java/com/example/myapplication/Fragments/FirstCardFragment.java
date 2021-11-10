2beb5500c4f654580fc2a33c2b92af4ac9f10573
AynurKhusainov <aynur8275@gmai.com>
2021-11-10 10:03:17 +0300

    asda

--- app/src/main/java/com/example/myapplication/Fragments/FirstCardFragment.java
+++ app/src/main/java/com/example/myapplication/Fragments/FirstCardFragment.java
@@ -1,7 +1,9 @@
 package com.example.myapplication.Fragments;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -15,14 +17,18 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.myapplication.Banks.Banks;
 import com.example.myapplication.R;
 
+import org.w3c.dom.Text;
+
 public class FirstCardFragment extends Fragment {
 
     static View v;
-    View  viewAlert;
+    View viewAlert;
     Button buttonSave;
     ProgressBar progressBarDataUser;
     AlertDialog alertDialog;
@@ -31,10 +37,10 @@ public class FirstCardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        if(v == null)
-            v= inflater.inflate(R.layout.fragment_first_card, container, false);
+        if (v == null)
+            v = inflater.inflate(R.layout.fragment_first_card, container, false);
 
-        ImageButton history=v.findViewById(R.id.historyOperation);
+        ImageButton history = v.findViewById(R.id.historyOperation);
 
         history.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,15 +48,16 @@ public class FirstCardFragment extends Fragment {
                 startActivity(new Intent(getActivity(), Banks.class));
             }
         });
-        ImageButton block=v.findViewById(R.id.Block);
+        ImageButton block = v.findViewById(R.id.Block);
 
         block.setOnClickListener(new View.OnClickListener() {
 
             EditText password;
             String text;
+
             @Override
             public void onClick(View v) {
-                text="qqq";
+                text = "qqq";
 
                 AlertDialog.Builder builder = new AlertDialog.Builder(getContext(), R.style.Theme_MyAlertDialog);
                 LayoutInflater inflater = getLayoutInflater();
@@ -60,23 +67,24 @@ public class FirstCardFragment extends Fragment {
                 builder.setView(viewAlert).setCancelable(true);
                 alertDialog = builder.create();
                 alertDialog.show();
-                password=viewAlert.findViewById(R.id.passwordd);
+                password = viewAlert.findViewById(R.id.passwordd);
 
                 buttonSave.setOnClickListener(vSave -> {
-                    if (password.getText().toString().equals(text)) {
+                    if (password.getText().toString().equals("123")) {
                         RelativeLayout s = FirstCardFragment.v.findViewById(R.id.functions);
                         s.setVisibility(View.GONE);
+                    } else {
+                        alertDialog.dismiss();
                     }
-                    else{
-                    alertDialog.dismiss();}
                 });
             }
         });
 
+        ImageButton rename = v.findViewById(R.id.Rename);
 
-        ImageButton rename=v.findViewById(R.id.Rename);
 
         rename.setOnClickListener(new View.OnClickListener() {
+            EditText password;
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(getContext(), R.style.Theme_MyAlertDialog);
@@ -87,8 +95,20 @@ public class FirstCardFragment extends Fragment {
                 builder.setView(viewAlert).setCancelable(true);
                 alertDialog = builder.create();
                 alertDialog.show();
+                password = viewAlert.findViewById(R.id.passwordd);
+
+                TextView name=FirstCardFragment.v.findViewById(R.id.cardname);
 
                 buttonSave.setOnClickListener(vSave -> {
+
+                    SharedPreferences.Editor editor = getContext().getSharedPreferences("SHARED_NAME_STRING", Context.MODE_PRIVATE).edit();
+                    editor.putString(name.toString(), password.getText().toString());
+                    editor.apply();
+
+
+                    SharedPreferences prefs = getContext().getSharedPreferences("SHARED_NAME_STRING", Context.MODE_PRIVATE);
+                    String loadedString = prefs.getString(name.toString(), null);
+                    name.setText(password.getText().toString());
                     alertDialog.dismiss();
                 });
             }
@@ -97,4 +117,5 @@ public class FirstCardFragment extends Fragment {
 
         return v;
     }
+
 }
